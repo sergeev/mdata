b52b41fdbbcbea115874ecb74be874c6fc14fc3c
sergeev <v.sergeev.m@gmail.com>
2019-03-10 23:18:56 +0700

    Create config http server and more...

--- main.go
+++ main.go
@@ -1,16 +1,110 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"log"
 	"net/http"
+	"strings"
+	"time"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello World</h1>")
+	fmt.Fprintf(w, "<h1>Hello World</h1>")
 }
 
 func main() {
-	fmt.Println("Server is listening...\n Server used port:3000")
-	http.HandlerFunc("/", indexHandler)
-	http.ListenAndServe(":3000", nil)
+
+	handler := http.NewServeMux()
+
+	// C R U D
+	handler.HandleFunc("/hello/", Logger(helloHandler))
+
+	handler.HandleFunc("/book/", Logger(bookHandler))
+
+	// server configs
+	s := http.Server{
+		Addr:           "0.0.0.0:8000",
+		Handler:        handler,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		IdleTimeout:    10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	// Test log server witch fatal error
+	log.Fatal(s.ListenAndServe())
+}
+
+type Resp struct {
+	Message string
+	Error   string
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	name := strings.Replace(r.URL.Path, "/hello/", "", 1)
+
+	resp := Resp{
+		Message: fmt.Sprintf("hello %s. Glad to see you again", name),
+	}
+
+	respJson, _ := json.Marshal(resp)
+
+	w.WriteHeader(http.StatusOK)
+
+	w.Write(respJson)
+}
+
+func Logger(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		log.Printf("Server [net/http] method [%s] connection from [%v]", r.Method, r.RemoteAddr)
+
+		next.ServeHTTP(w, r)
+	}
+}
+
+func bookHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		handleGetBook(w, r)
+	}
+}
+
+func handleGetBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+type Book struct {
+	Id     string `json:"id"`
+	Author string `json:"author"`
+	Name   string `json:"name"`
+}
+
+type BookStore struct {
+	books []Book
+}
+
+func (s BookStore) FindBookById(id string) *Book {
+	for _, book := range s.books {
+		if book.Id == id {
+			return &book
+		}
+	}
+	return nil
+}
+
+func (s BookStore) GetBooks() []Book {
+	return s.books
+}
+
+func (s *BookStore) UpdateBook(book Book) error {
+	for i, bk := range s.books {
+		if bk.Id == book.Id {
+			s.books[i] = book
+			return nil
+		}
+	}
+	return errors.New(fmt.Sprintf("Book with id %s not found", book.Id))
 }
